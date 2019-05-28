1ec26da9871ef26b99729ac1af44f86a0bbf1430
Nuttapon Kongkitimanon <nuttapon.k@appman.co.th>
2019-05-28 09:00:51 +0700

    create test suit for sum

---
+++ sum.js
@@ -0,0 +1,11 @@
+function sum1(a,b){
+	return 1
+}
+
+function sum2b(b){
+  return b + b
+}
+
+module.export = {
+	sum1, sum2b
+}
---
+++ sum.test.js
@@ -0,0 +1,5 @@
+const { sum1, sum2b } = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum1( )).toBe(3);
+});
